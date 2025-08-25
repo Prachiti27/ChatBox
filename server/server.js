0ebe0be8fc05e321af3bef4863880ecbd343f023
Prachiti Kitey <prachitikitey86@gmail.com>
2025-08-25 18:14:35 +0530

    major update

--- server/server.js
+++ server/server.js
@@ -39,8 +39,8 @@ app.use("/api/messages",messageRouter)
 
 await connectDB()
 
-const PORT = process.env.PORT || 5000
-server.listen(PORT,()=>
-    console.log(`Server is running on port:${PORT}`)
-)
+// const PORT = process.env.PORT || 5000
+// server.listen(PORT,()=>
+//     console.log(`Server is running on port:${PORT}`)
+// )
 
