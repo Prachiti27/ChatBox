9ac259be42179a65ee85f7bbada9ed60d7c5bd47
Prachiti Kitey <prachitikitey86@gmail.com>
2025-08-25 18:19:24 +0530

    major update

--- server/server.js
+++ server/server.js
@@ -10,37 +10,42 @@ import { Server } from 'socket.io'
 const app = express()
 const server = http.createServer(app)
 
-export const io = new Server(server,{
-    cors: {origin: "*"}
+export const io = new Server(server, {
+    cors: { origin: "*" }
 })
 
 export const userSocketMap = {}
 
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId
-    console.log("User connected",userId)
+    console.log("User connected", userId)
 
-    if(userId) userSocketMap[userId] = socket.id
-    io.emit("getOnlineUsers",Object.keys(userSocketMap))
+    if (userId) userSocketMap[userId] = socket.id
+    io.emit("getOnlineUsers", Object.keys(userSocketMap))
 
-    socket.on("disconnect",()=>{
-        console.log("User disconnected",userId)
+    socket.on("disconnect", () => {
+        console.log("User disconnected", userId)
         delete userSocketMap[userId]
-        io.emit("getOnlineUsers",Object.keys(userSocketMap))
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
     })
 })
 
-app.use(express.json({limit: "4mb"}))
+app.use(express.json({ limit: "4mb" }))
 app.use(cors())
 
-app.use("/api/status",(req,res)=>res.send("Server is live"))
-app.use("/api/auth",userRouter)
-app.use("/api/messages",messageRouter)
+app.use("/api/status", (req, res) => res.send("Server is live"))
+app.use("/api/auth", userRouter)
+app.use("/api/messages", messageRouter)
 
 await connectDB()
 
-// const PORT = process.env.PORT || 5000
-// server.listen(PORT,()=>
-//     console.log(`Server is running on port:${PORT}`)
-// )
+
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5000
+    server.listen(PORT, () =>
+        console.log(`Server is running on port:${PORT}`)
+    )
+}
+
+export default server
 
